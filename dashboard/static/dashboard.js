0bec126fb43cedabf95d68de6cb3b962a8632653
ninabws <nina.bianca06@aluno.ifce.edu.br>
2023-05-04 16:54:15 -0300

    .

--- dashboard/static/dashboard.js
+++ dashboard/static/dashboard.js
@@ -5,6 +5,7 @@
 
   feather.replace({ 'aria-hidden': 'true' })
 
+  
   // Graphs
   const ctx = document.getElementById('myChart')
   // eslint-disable-next-line no-unused-vars
@@ -12,24 +13,16 @@
     type: 'line',
     data: {
       labels: [
-        'Sunday',
-        'Monday',
-        'Tuesday',
-        'Wednesday',
-        'Thursday',
-        'Friday',
-        'Saturday'
+        'Domingo',
+        'Segunda',
+        'Terça',
+        'Quarta',
+        'Quinta',
+        'Sexta',
+        'Sabado'
       ],
       datasets: [{
-        data: [
-          15339,
-          21345,
-          18483,
-          24003,
-          23489,
-          24092,
-          12034
-        ],
+        data: dt,
         lineTension: 0,
         backgroundColor: 'transparent',
         borderColor: '#007bff',
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http import request
 # Create your views here.
+dt1 = [
+      4,
+      21,
+      13,
+      4,
+      18,
+      92,
+      0
+    ]
 
 def index (request):
-    return render (request, 'index.html')
+    return render (request, 'index.html', {'dt1': dt1})
